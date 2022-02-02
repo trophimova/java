a62ab87340655f7dc362b9767ecac81bf76dfca7
Anya <48622247+trophimova@users.noreply.github.com>
2022-02-02 21:26:11 +0300

    Create Lesson6.java

---
+++ Lesson6/Lesson6.java
@@ -0,0 +1,97 @@
+/**
+ * Java 1. Homework 6
+ * 
+ * @author Anna Trofimova
+ * @version 2.2.2022
+ */
+
+class Lesson6 {
+    public static void main(String[] args) {
+        Cat cat = new Cat(200);
+        Dog dog = new Dog(500, 10);
+
+        IAnimal[] animals = { cat, dog };
+        for (IAnimal animal : animals) {
+            System.out.println(animal);
+            System.out.println(animal.run(150));
+            System.out.println(animal.run(250));
+            System.out.println(animal.run(550));
+            System.out.println(animal.swim(5));
+            System.out.println(animal.swim(15));
+        }
+        System.out.println("Animals were created: " + Animal.getCountOfAnimals());
+    }
+}
+
+class Cat extends Animal {
+    Cat(int runLimit, int swimLimit) {
+        super(runLimit, swimLimit);
+    }
+
+    Cat(int runLimit) {
+        super(runLimit, -1);
+    }
+
+    @Override
+    public String swim(int distance) {
+        return getClassName() + " can't swim";
+    }
+
+}
+
+class Dog extends Animal {
+    Dog(int runLimit, int swimLimit) {
+        super(runLimit, swimLimit);
+    }
+}
+
+interface IAnimal {
+    String run(int distance);
+
+    String swim(int distance);
+}
+
+abstract class Animal implements IAnimal {
+    protected int runLimit;
+    protected int swimLimit;
+    protected String className;
+    protected static int countOfAnimals = 0;
+
+    Animal(int runLimit, int swimLimit) {
+        this.runLimit = runLimit;
+        this.swimLimit = swimLimit;
+        className = getClass().getSimpleName();
+        countOfAnimals++;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public static int getCountOfAnimals() {
+        return countOfAnimals;
+    }
+
+    @Override
+    public String run(int distance) {
+        if (distance > runLimit) {
+            return className + " couldn't run " + distance;
+        } else {
+            return className + " successfully ran " + distance;
+        }
+    }
+
+    @Override
+    public String swim(int distance) {
+        if (distance > swimLimit) {
+            return className + " couldn't swim " + distance;
+        } else {
+            return className + " successfully swam " + distance;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return className + ". runLimit: " + runLimit + ", swimLimit: " + swimLimit;
+    }
+}
